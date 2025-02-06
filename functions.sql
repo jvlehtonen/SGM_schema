946207679b18342b92d63f1f9e0372561a987e9a
Jukka V. Lehtonen <jukka.lehtonen@abo.fi>
2025-02-06 16:27:26 +0200

    Add two helper functions for data insertion

--- functions.sql
+++ functions.sql
@@ -18,4 +18,37 @@ BEGIN
 END //
 
 
+CREATE OR REPLACE FUNCTION variant2resnum(val varchar(6)) RETURNS smallint(5) unsigned
+BEGIN
+  DECLARE resnum int;
+  SET resnum = SUBSTRING( val, 2, LENGTH(val)-2 );
+  RETURN resnum;
+END //
+
+
+CREATE OR REPLACE FUNCTION later_residue(val varchar(20)) RETURNS varchar(6)
+BEGIN
+  DECLARE res varchar(6);
+  DECLARE one varchar(6);
+  DECLARE two varchar(6);
+  DECLARE commapos int;
+  DECLARE rone int;
+  DECLARE rtwo int;
+  SET commapos = locate(', ', val);
+  IF 0 < commapos
+  THEN
+    SET one = substring(val, 1, commapos - 1);
+    SET rone = variant2resnum(one);
+    SET two = substring(val, commapos + 2);
+    SET rtwo = variant2resnum(two);
+    IF rone < rtwo
+    THEN SET res = two;
+    ELSE SET res = one;
+    END IF;
+  ELSE SET res = val;
+  END IF;
+  RETURN res;
+END //
+
+
 DELIMITER ;
